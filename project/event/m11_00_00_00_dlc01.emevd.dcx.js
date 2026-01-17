dae82b1e05b5cbb9c0d0bedd8c2b18466b226f8a
Lily <40526330+Umgak@users.noreply.github.com>
2026-01-17 00:37:01 -0600

    remove unmodified scripts No reason to have them here.

--- project/event/m11_00_00_00_dlc01.emevd.dcx.js
+++
@@ -1,9 +0,0 @@
-// ==EMEVD==
-// @docs    nr-common.emedf.json
-// @compress    DCX_KRAK
-// @game    Sekiro
-// @string    "W:\\CL\\data\\Param\\event\\common_func.emevd\u0000W:\\CL\\data\\Param\\event\\common_macro.emevd\u0000\u0000\u0000\u0000\u0000\u0000"
-// @linked    [0,82]
-// @version    3.6.2
-// ==/EMEVD==
-
