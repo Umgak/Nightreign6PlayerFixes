a66e7aa7c0bc78d63757b26e6bfad184bb0b8509
Lily <40526330+Umgak@users.noreply.github.com>
2026-01-18 21:56:26 -0600

    commit already fixed script I keep accidentally checking it

--- project/script/762100_battle-luabnd-dcx/762100_battle.lua
+++ project/script/762100_battle-luabnd-dcx/762100_battle.lua
@@ -670,7 +670,8 @@ function AngelsDaughters762100_Act49(ai, goal, paramTbl)
     elseif ai:HasSpecialEffectId(TARGET_ENE_1, 60730) == true then
         goal:AddSubGoal(GOAL_COMMON_ApproachSettingDirection, ai:GetRandam_Int(1, 2), TARGET_ENE_1, 10, TARGET_SELF, true, -1, AI_DIR_TYPE_ToR, ai:GetRandam_Int(4, 5))
     elseif ai:HasSpecialEffectId(TARGET_ENE_2, 60730) == true then
-        goal:AddSubGoal(GOAL_COMMON_ApproachSettingDirection, ai:GetRandam_Int(1, 2), TARGET_ENE_2, 10, TARGET_SELF, true, -1, AI_DIR_TYPE_ToL, ai:GetRandam_Int(4, 5)) -- NR6PF: Why do I have to fix Fromsoft's errors too? Correctly target TARGET_ENE_3
+        -- NR6PF: Why do I have to fix Fromsoft's errors too? Correctly target TARGET_ENE_2
+        goal:AddSubGoal(GOAL_COMMON_ApproachSettingDirection, ai:GetRandam_Int(1, 2), TARGET_ENE_2, 10, TARGET_SELF, true, -1, AI_DIR_TYPE_ToL, ai:GetRandam_Int(4, 5))
         -- NR6PF: Also target P4-6 if they get the effect, blah blah
     elseif ai:HasSpecialEffectId(TARGET_ENE_3, 60730) == true then
         goal:AddSubGoal(GOAL_COMMON_ApproachSettingDirection, ai:GetRandam_Int(1, 2), TARGET_ENE_3, 10, TARGET_SELF, true, -1, AI_DIR_TYPE_ToR, ai:GetRandam_Int(4, 5))
